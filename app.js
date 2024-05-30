7282a7d30f56939e7988c2a86a55d18eeeed2973
haiaza <ajames1342@gmail.com>
2024-05-29 20:47:24 -0400

    ?

--- app.js
+++ app.js
@@ -1,75 +1,65 @@
 /*-------------------------------- Constants --------------------------------*/
-const buttons = document.querySelectorAll('.button')
-const display = document.querySelector('.display')
-const operators  = document.querySelectorAll('.operator')
+let num1 = "";
+let num2 = "";
+let numChoice = "";
+let opChoice = "";
 
-const calculator = document.querySelector('#calculator');
-
-let firstNumber = null;
-let secondNumber = null;
-let chosenOperator = null;
-
-/*-------------------------------- Variables --------------------------------*/
-buttons.forEach((button) => {
-    button.addEventListener('click', (event) => {
-      // This log is for testing purposes to verify we're getting the correct value
-      console.log(event.target.innerText);
-      // Future logic to capture the button's value would go here...
-      const value
-    });
-  });
-  // testing
-/*------------------------ Cached Element References ------------------------*/
-
-/*----------------------------- Event Listeners -----------------------------*/
-calculator.addEventListener('click', (event) => {
-  // This log is for testing purposes to verify we're getting the correct value
-  // You have to click a button to see this log
-  console.log(event.target.innerText);
-
-  // Example
-  if (event.target.classList.contains('number')) {
-    // Do something with a number
-    let num1;
-    num1  = event.target.innerText
-    console.log(num1)
-  }
-
-  // Example
-  if (event.target.innerText === '*') {
-    // Do something with this operator
-    operator = '*'
-  }
-});
-console.log(operator)
-/*-------------------------------- Functions --------------------------------*/
-function calculation(num1,num2) {
-  if (num1 == null) {
-    console.log('please click a number');
-  }else {
-
-  }
+const render = () =>{
+  display.textContent = result
 }
 
-// im thinking the function should take 3 args (input1, operator, input2)
-// or maybe the function callback will just incorporate all three with special logic for the operator
+const updateResult = (event) => {
+  result = num1
+  render()
+}
 
-// As a user, I want to be able to select numbers so that I can perform operations with them.
-/*  */
+const updateNumbers = (event) => {
+  if(num1){
+    num1 = event.target.textContent
+  }else{
+    num1 = event.target.textContent
 
-// As a user, I want to be able to add two numbers together.
-/*   */
+    updateResult()
+  }
+}
+/*-------------------------------- Variables --------------------------------*/
 
-// As a user, I want to be able to subtract one number from another.
-/*  */
+/*------------------------ Cached Element References ------------------------*/
+const btnEls = document.querySelectorAll(".button");
+const numberEls = document.querySelectorAll(".number");
+const operatorEls = document.querySelectorAll(".operator");
+const equalsEl = document.querySelector(".equals");
+const display = document.querySelector(".display");
 
-// As a user, I want to be able to multiply two numbers together.
+const calculator = document.querySelector("#calculator");
+/*----------------------------- Event Listeners -----------------------------*/
+btnEls.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    //testing purposes
+    // console.log(event.target.innerText);
+  });
+});
+
+calculator.addEventListener("click", (event) => {
+  // testing purposes
+  // console.log(event.target.innerText);
+  if (event.target.classList.contains("number")) {
+    numberClicked(event.target.innerText);
+    //function for numbers
+    //must update the display
+  }
+});
+/*-------------------------------- Functions --------------------------------*/
 
 
-// As a user, I want to be able to divide one number by another.
+function numberClicked(number) {
+  numChoice += number;
+  display.innerText = numChoice;
+}
 
+function opClicked(operator) {}
 
-// As a user, I want to be able to see the output of the mathematical operation.
-
-
-// As a user, I want to be able to clear all operations and start from 0.
+/* mental notes*/
+//At this point in the code we can select numbers. When we do they update the display
+//Pressing a op does nothing.
+//Technically the numbers inside of the display are strings.
