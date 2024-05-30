191c86102e47110150ebaab8292e5d8837c14d2f
haiaza <ajames1342@gmail.com>
2024-05-29 22:02:12 -0400

    redoing the code and starting again.

--- app.js
+++ app.js
@@ -1,65 +1,22 @@
-/*-------------------------------- Constants --------------------------------*/
-let num1 = "";
-let num2 = "";
-let numChoice = "";
-let opChoice = "";
+let num1;
+let num2;
+let operator;
+let result = 0;
 
-const render = () =>{
-  display.textContent = result
+const numberBtnEls = document.querySelectorAll('.number')
+const operatorBtnEls = document.querySelectorAll('.operator')
+const equalBtnEl = document.querySelector('.equals')
+const display = document.querySelector('.display')
+
+
+//functions
+const updateNumber = (event) => {
+  console.log(event.target.textContent)
+  display.textContent = event.target.textContent
 }
 
-const updateResult = (event) => {
-  result = num1
-  render()
-}
 
-const updateNumbers = (event) => {
-  if(num1){
-    num1 = event.target.textContent
-  }else{
-    num1 = event.target.textContent
-
-    updateResult()
-  }
-}
-/*-------------------------------- Variables --------------------------------*/
-
-/*------------------------ Cached Element References ------------------------*/
-const btnEls = document.querySelectorAll(".button");
-const numberEls = document.querySelectorAll(".number");
-const operatorEls = document.querySelectorAll(".operator");
-const equalsEl = document.querySelector(".equals");
-const display = document.querySelector(".display");
-
-const calculator = document.querySelector("#calculator");
-/*----------------------------- Event Listeners -----------------------------*/
-btnEls.forEach((btn) => {
-  btn.addEventListener("click", (event) => {
-    //testing purposes
-    // console.log(event.target.innerText);
-  });
-});
-
-calculator.addEventListener("click", (event) => {
-  // testing purposes
-  // console.log(event.target.innerText);
-  if (event.target.classList.contains("number")) {
-    numberClicked(event.target.innerText);
-    //function for numbers
-    //must update the display
-  }
-});
-/*-------------------------------- Functions --------------------------------*/
-
-
-function numberClicked(number) {
-  numChoice += number;
-  display.innerText = numChoice;
-}
-
-function opClicked(operator) {}
-
-/* mental notes*/
-//At this point in the code we can select numbers. When we do they update the display
-//Pressing a op does nothing.
-//Technically the numbers inside of the display are strings.
+//event handlers
+numberBtnEls.forEach((numberBtnEl) =>{
+  numberBtnEl.addEventListener('click', updateNumber)
+})
